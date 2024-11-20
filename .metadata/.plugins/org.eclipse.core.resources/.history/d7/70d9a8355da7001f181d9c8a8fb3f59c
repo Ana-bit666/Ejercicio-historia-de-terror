99769e2a746f22b74d748f22194e667ec1673862
Mito <jaimitom1410@gmail.com>
2024-11-20 17:34:33 +0100

    borrador

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d7/70d9a8355da7001f181d9c8a8fb3f59c
@@ -0,0 +1,10 @@
+package borrador;
+
+public class Borrador_historia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- historia_de_terror/src/borrador/Borrador_historia.java
+++ historia_de_terror/src/borrador/Borrador_historia.java
@@ -3,8 +3,12 @@ package borrador;
 public class Borrador_historia {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hace muchos años, en Irlanda, en una mansion alejada, \n"
+				+ "ocurrio un asesinato, una niña fue brutalmente asesinada \n"
+				+ "en un puente cercano, donde el asesino se dio a la fuga,\n"
+				+ "el padre de la niña estaba gravemente enfermo y tras la noticia, y fallecio a \n"
+				+ "los pocos meses, hasta qué, Jose, el hermano del padre se mudo acompañado de su hijo \n"
+				+ " a la mansión");
 	}
 
 }
